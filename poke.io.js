0f1620d962c8733a596b449ad862d3d181868b00
Martín Rafael González <tin@devtin.io>
2016-07-28 22:59:46 -0400

    treating any email address as a google account

--- poke.io.js
+++ poke.io.js
@@ -151,7 +151,7 @@ function Pokeio() {
     }
 
     self.init = function (username, password, location) {
-        self.playerInfo.provider = /@gmail\.com$/i.test(username) ? 'google' : 'ptc';
+        self.playerInfo.provider = /@/i.test(username) ? 'google' : 'ptc'; // treat any email address as a google account
 
         return self.SetLocation(location)
             .then(() => self.GetAccessToken(username,password))
